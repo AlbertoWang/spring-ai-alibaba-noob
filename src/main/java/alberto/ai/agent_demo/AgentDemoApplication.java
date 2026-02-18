dd4309cd183f5abdbd171b814d4504e7987741f2
AlbertoWang <albertowang@foxmail.com>
2026-02-18 18:30:59 +0800

    feat:controller请求

--- src/main/java/alberto/ai/agent_demo/AgentDemoApplication.java
+++ src/main/java/alberto/ai/agent_demo/AgentDemoApplication.java
@@ -2,8 +2,10 @@ package alberto.ai.agent_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"alberto.ai"})
 public class AgentDemoApplication {
 
 	public static void main(String[] args) {
