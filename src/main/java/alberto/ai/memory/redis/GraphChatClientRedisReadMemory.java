bf5564e0589ab9eb1a35e55754d8f8f848d3374d
AlbertoWang <albertowang@foxmail.com>
2026-02-19 19:25:27 +0800

    feat:redis memory

--- src/main/java/alberto/ai/memory/redis/GraphChatClientRedisReadMemory.java
+++ src/main/java/alberto/ai/memory/redis/GraphChatClientRedisReadMemory.java
@@ -1,37 +1,42 @@
-package alberto.ai.memory;
+package alberto.ai.memory.redis;
 
+import alberto.ai.memory.GraphChatClient;
 import com.alibaba.cloud.ai.graph.CompiledGraph;
 import com.alibaba.cloud.ai.graph.OverAllState;
 import com.alibaba.cloud.ai.graph.RunnableConfig;
 import com.alibaba.cloud.ai.graph.exception.GraphStateException;
+import com.alibaba.cloud.ai.graph.state.StateSnapshot;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 /**
- * 使用文件系统持久化的 Memory
- *
  * @author albertowang@foxmail.com
- * @date 2026/2/18 18:44
+ * @date 2026/2/19 19:17
  **/
 
-public class GraphChatClientFileSystemMemory {
+public class GraphChatClientRedisReadMemory {
     public static void main(String[] args) throws GraphStateException {
-        CompiledGraph graph = GraphChatClient.getFileSystemGraph("/Users/alberto/CodingSpace/IDEA-workspace/agent-memory");
+        Config redisConfig = new Config();
+        redisConfig.useSingleServer().setAddress("redis://localhost:6379");
+        RedissonClient redisson = Redisson.create(redisConfig);
+
+        CompiledGraph graph = GraphChatClient.getRedisGraph(redisson);
 
         RunnableConfig config = RunnableConfig.builder()
                 .threadId("1")
                 .build();
 
-        graph.invoke(Map.of("messages", List.of("我是 Bob")), config)
-                .map(OverAllState::data)
-                .map(data -> data.get("messages"))
-                .ifPresent(l -> {
-                    List<String> list = (List<String>) l;
-                    list.forEach(System.out::println);
-                });
+        Collection<StateSnapshot> stateHistory = graph.getStateHistory(config);
+        System.out.println(stateHistory);
+        System.out.println("===========");
 
-        System.out.println("=======");
+        StateSnapshot state = graph.getState(config);
+        System.out.println(state);
 
         graph.invoke(Map.of("messages", List.of("我的名字是什么？")), config)
                 .map(OverAllState::data)
--- src/main/java/alberto/ai/memory/redis/GraphChatClientRedisWriteMemory.java
+++ src/main/java/alberto/ai/memory/redis/GraphChatClientRedisWriteMemory.java
@@ -1,39 +1,40 @@
-package alberto.ai.memory;
+package alberto.ai.memory.redis;
 
+import alberto.ai.memory.GraphChatClient;
 import com.alibaba.cloud.ai.graph.CompiledGraph;
 import com.alibaba.cloud.ai.graph.OverAllState;
 import com.alibaba.cloud.ai.graph.RunnableConfig;
 import com.alibaba.cloud.ai.graph.exception.GraphStateException;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 
 import java.util.List;
 import java.util.Map;
 
 /**
- * 使用文件系统持久化的 Memory
+ * 1. 新建 docker 镜像：docker run -d --name my-redis -p 6379:6379 redis:latest
+ * 2. 进入 docker 容器：docker exec -it my-redis redis-cli
+ * 3. 关闭 docker 容器：docker stop my-redis
+ * 4. 启动 docker 容器：docker start my-redis
  *
  * @author albertowang@foxmail.com
- * @date 2026/2/18 18:44
+ * @date 2026/2/19 18:28
  **/
 
-public class GraphChatClientFileSystemMemory {
+public class GraphChatClientRedisWriteMemory {
     public static void main(String[] args) throws GraphStateException {
-        CompiledGraph graph = GraphChatClient.getFileSystemGraph("/Users/alberto/CodingSpace/IDEA-workspace/agent-memory");
+        Config redisConfig = new Config();
+        redisConfig.useSingleServer().setAddress("redis://localhost:6379");
+        RedissonClient redisson = Redisson.create(redisConfig);
+
+        CompiledGraph graph = GraphChatClient.getRedisGraph(redisson);
 
         RunnableConfig config = RunnableConfig.builder()
                 .threadId("1")
                 .build();
 
         graph.invoke(Map.of("messages", List.of("我是 Bob")), config)
-                .map(OverAllState::data)
-                .map(data -> data.get("messages"))
-                .ifPresent(l -> {
-                    List<String> list = (List<String>) l;
-                    list.forEach(System.out::println);
-                });
-
-        System.out.println("=======");
-
-        graph.invoke(Map.of("messages", List.of("我的名字是什么？")), config)
                 .map(OverAllState::data)
                 .map(data -> data.get("messages"))
                 .ifPresent(l -> {
